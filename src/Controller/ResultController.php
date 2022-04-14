99b5b6819b54bae8ea3e019ea90b317ef11cf7fd
EmmanuelleAllain <e.allain13@gmail.com>
2022-04-14 16:53:15 +0200

    architecture in files

---
+++ src/Controller/ResultController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ResultManager;
+
+class ResultController extends AbstractController
+{
+    //function show
+    // function calc result
+    // appel twig page
+}
