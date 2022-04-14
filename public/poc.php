2938257768745f669622e20ae506aac0b1cbd502
ghartemann <g.hartemann@gmail.com>
2022-04-14 16:58:31 +0200

    corrected a few lines thanks to grump

--- public/poc.php
+++ public/poc.php
@@ -5,7 +5,8 @@ $questions = [
     "title" => "À quoi ressemble votre régime alimentaire habituel ?",
     "choice1" => "viande plus de trois fois par semaine",
     "choice2" => "viande jusqu'à trois fois par semaine",
-    "choice3" => "végétarien (pas de viande, mais des produits d’origine animale comme les oeufs, le lait ou le fromage)",
+    "choice3" => "végétarien (pas de viande, mais des produits d’origine animale
+    comme les oeufs, le lait ou le fromage)",
     "choice4" => "végétalien (pas de viande ni de produits d’origine animale)",
     "result1" => "100",
     "result2" => "200",
@@ -188,7 +189,8 @@ $questions = [
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  $ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>POC Questionnaire</title>
 </head>
 
@@ -198,27 +200,31 @@ $questions = [
     <?php
     $i = 0;
     foreach ($questions as $question) {
-      $i++;
-    ?>
+        $i++;
+        ?>
 
-      <div>
+            <div>
         <p>
           <label for="q<?= $i ?>"><?= $question["title"] ?></label>
         </p>
         <p class="form-check">
-          <input class="form-check-input" type="radio" id="choice1" name="q<?= $i ?>" value="<?= $question["result1"] ?>">
+          <input class="form-check-input" type="radio" id="choice1" name="q<?= $i ?>"
+          value="<?= $question["result1"] ?>">
           <label class="form-check-label" for="q<?= $i ?>"><?= $question["choice1"] ?></label>
         </p>
         <p class="form-check">
-          <input class="form-check-input" type="radio" id="choice2" name="q<?= $i ?>" value="<?= $question["result2"] ?>">
+          <input class="form-check-input" type="radio" id="choice2" name="q<?= $i ?>"
+          value="<?= $question["result2"] ?>">
           <label class="form-check-label" for="q<?= $i ?>"><?= $question["choice2"] ?></label>
         </p>
         <p class="form-check">
-          <input class="form-check-input" type="radio" id="choice3" name="q<?= $i ?>" value="<?= $question["result3"] ?>">
+          <input class="form-check-input" type="radio" id="choice3" name="q<?= $i ?>"
+          value="<?= $question["result3"] ?>">
           <label class="form-check-label" for="q<?= $i ?>"><?= $question["choice3"] ?></label>
         </p>
         <p class="form-check">
-          <input class="form-check-input" type="radio" id="choice4" name="q<?= $i ?>" value="<?= $question["result4"] ?>">
+          <input class="form-check-input" type="radio" id="choice4" name="q<?= $i ?>"
+          value="<?= $question["result4"] ?>">
           <label class="form-check-label" for="q<?= $i ?>"><?= $question["choice4"] ?></label>
         </p>
       </div>
