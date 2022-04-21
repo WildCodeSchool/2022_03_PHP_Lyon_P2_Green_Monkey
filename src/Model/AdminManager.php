6c3ce9de640065eed97bdbcff5253f4d3bf8c640
EmmanuelleAllain <e.allain13@gmail.com>
2022-04-21 13:49:21 +0200

    correct sending float numbers in sql request

--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -10,7 +10,7 @@ class AdminManager extends AbstractManager
     {
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `answer_value` = :answer_value WHERE id=:id");
         $statement->bindValue('id', $value['id'], \PDO::PARAM_INT);
-        $statement->bindValue('answer_value', $value['answer_value'], \PDO::PARAM_INT);
+        $statement->bindValue('answer_value', $value['answer_value'], \PDO::PARAM_STR);
 
         return $statement->execute();
     }
