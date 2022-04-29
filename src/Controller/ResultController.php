28bea0094c9b30e203c63592cb514482ec73bde3
ghartemann <g.hartemann@gmail.com>
2022-04-29 11:36:07 +0200

    initial commit ; finished functionality

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -9,6 +9,9 @@ class ResultController extends AbstractController
 {
     public function index(): string
     {
+        // adding data to db
+        $this->add();
+
         // fetching data from database and comparing it with POST
         $resultManager = new ResultManager();
         $values = $resultManager->fetchValuesByAnswer($_POST);
@@ -24,5 +27,15 @@ class ResultController extends AbstractController
             'totalFootprint' => $totalFootprint,
             'footprintByCategory' => $footprintByCategory
         ]);
+    }
+
+    public function add(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $answers = array_map('trim', $_POST);
+
+            $answersManager = new ResultManager();
+            $answersManager->insert($answers);
+        }
     }
 }
--- src/Model/ResultManager.php
+++ src/Model/ResultManager.php
@@ -24,4 +24,23 @@ class ResultManager extends AbstractManager
         }
         return $values;
     }
+
+    public function insert(array $answers): void
+    {
+        // preparing query
+        $toSave = "q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16";
+        $placeholders = ":q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10, :q11, :q12, :q13, :q14, :q15, :q16";
+        $statement = $this->pdo->prepare("INSERT INTO user_results (" . $toSave . ") VALUES (" . $placeholders . ")");
+
+        // binding values
+        $increment = 1;
+        foreach ($answers as $answer) {
+            $valueToBind = "q" . $increment;
+            $statement->bindValue($valueToBind, $answer, \PDO::PARAM_STR);
+            $increment++;
+        }
+
+        // executing said query
+        $statement->execute();
+    }
 }
