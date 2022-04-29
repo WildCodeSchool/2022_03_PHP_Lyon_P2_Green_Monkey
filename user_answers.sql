f5c17224821b47b3ca504c7a6d3242ccd5c092b3
ghartemann <g.hartemann@gmail.com>
2022-04-29 14:21:24 +0200

    added sql file

---
+++ user_answers.sql
@@ -0,0 +1,26 @@
+create table projet2.user_results
+(
+    id  int auto_increment
+        primary key,
+    q1  varchar(10) not null,
+    q2  varchar(10) not null,
+    q3  varchar(10) not null,
+    q4  varchar(10) not null,
+    q5  varchar(10) not null,
+    q6  varchar(10) not null,
+    q7  varchar(10) not null,
+    q8  varchar(10) not null,
+    q9  varchar(10) not null,
+    q10 varchar(11) not null,
+    q11 varchar(11) not null,
+    q12 varchar(11) not null,
+    q13 varchar(11) not null,
+    q14 varchar(11) not null,
+    q15 varchar(11) not null,
+    q16 varchar(11) not null
+);
+
+INSERT INTO projet2.user_results (id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (1, 'q1_choice2', 'q2_choice1', 'q3_choice2', 'q4_choice3', 'q5_choice3', 'q6_choice2', 'q7_choice1', 'q8_choice2', 'q9_choice2', 'q10_choice2', 'q11_choice1', 'q12_choice2', 'q13_choice2', 'q14_choice1', 'q15_choice2', 'q16_choice2');
+INSERT INTO projet2.user_results (id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (2, 'q1_choice1', 'q2_choice4', 'q3_choice2', 'q4_choice3', 'q5_choice3', 'q6_choice3', 'q7_choice1', 'q8_choice2', 'q9_choice4', 'q10_choice2', 'q11_choice2', 'q12_choice2', 'q13_choice4', 'q14_choice2', 'q15_choice3', 'q16_choice1');
+INSERT INTO projet2.user_results (id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (3, 'q1_choice2', 'q2_choice2', 'q3_choice3', 'q4_choice3', 'q5_choice3', 'q6_choice2', 'q7_choice2', 'q8_choice2', 'q9_choice1', 'q10_choice4', 'q11_choice2', 'q12_choice2', 'q13_choice2', 'q14_choice3', 'q15_choice3', 'q16_choice1');
+INSERT INTO projet2.user_results (id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (4, 'q1_choice2', 'q2_choice2', 'q3_choice2', 'q4_choice2', 'q5_choice2', 'q6_choice2', 'q7_choice1', 'q8_choice4', 'q9_choice2', 'q10_choice2', 'q11_choice3', 'q12_choice2', 'q13_choice2', 'q14_choice2', 'q15_choice2', 'q16_choice2');
