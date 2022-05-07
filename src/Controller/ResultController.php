fd3e666566358c4e958bf2056307bce038f2f043
ghartemann <g.hartemann@gmail.com>
2022-05-07 08:32:45 +0200

    finished ugly script which works anyway

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -9,29 +9,29 @@ class ResultController extends AbstractController
 {
     public function index(): ?string
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $resultManager = new ResultManager();
-            $resultService = new ResultService();
+        $resultManager = new ResultManager();
+        $resultService = new ResultService();
 
-            $answers = array_map('trim', $_POST);
-
-            // fetching data from database and comparing it with POST
-            $values = $resultManager->fetchValuesByAnswer($answers);
-
-            // calculating footprint (total and by category)
-            $totalFootprint = $resultService->calculateTotalFootprint($values);
-            $footprintByCategory = $resultService->calculateFootprintByCat($values);
-
-            // adding data to db
-            $resultManager->insert($answers, $totalFootprint, $footprintByCategory);
-
-            // rendering page with data we got earlier
-            return $this->twig->render('Results/results.html.twig', [
-                'values' => $values,
-                'totalFootprint' => $totalFootprint,
-                'footprintByCategory' => $footprintByCategory
-            ]);
+        for ($i = 1; $i < 17; $i++) {
+            $answers[] = "q" . $i . "_choice" . rand(1, 4);
         }
+
+        // fetching data from database and comparing it with POST
+        $values = $resultManager->fetchValuesByAnswer($answers);
+
+        // calculating footprint (total and by category)
+        $totalFootprint = $resultService->calculateTotalFootprint($values);
+        $footprintByCategory = $resultService->calculateFootprintByCat($values);
+
+        // adding data to db
+        $resultManager->insert($answers, $totalFootprint, $footprintByCategory);
+
+        // rendering page with data we got earlier
+        return $this->twig->render('Results/results.html.twig', [
+            'values' => $values,
+            'totalFootprint' => $totalFootprint,
+            'footprintByCategory' => $footprintByCategory
+        ]);
         header("location: calculator");
         return null;
     }
