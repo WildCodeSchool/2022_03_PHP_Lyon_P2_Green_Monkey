93608964dddc65ab9549a46bad7a8d969b5f68bb
ghartemann <g.hartemann@gmail.com>
2022-05-10 14:14:12 +0200

    still more fixes

--- public/assets/js/stats.js
+++ public/assets/js/stats.js
@@ -1,13 +1,13 @@
-const food = document.getElementById('food').textContent;
-const accommodation = document.getElementById('accommodation').textContent;
-const transport = document.getElementById('transport').textContent;
-const digital = document.getElementById('digital').textContent;
+const food = document.getElementById("food").textContent;
+const accommodation = document.getElementById("accommodation").textContent;
+const transport = document.getElementById("transport").textContent;
+const digital = document.getElementById("digital").textContent;
 
 let resultbyCategory = [
-    {'name' : 'Alimentation', 'value' : food },
-    {'name' : 'Logement', 'value' : accommodation },
-    {'name' : 'Transport', 'value' : transport },
-    {'name' : 'Numérique', 'value' : digital },
+    { name: "Alimentation", value: food },
+    { name: "Logement", value: accommodation },
+    { name: "Transport", value: transport },
+    { name: "Numérique", value: digital },
 ];
 
 const labels = [
@@ -15,29 +15,33 @@ const labels = [
     resultbyCategory[1].name,
     resultbyCategory[2].name,
     resultbyCategory[3].name,
-  ];
+];
 
 const data = {
-labels: labels,
-datasets: [{
-    backgroundColor: [
-    '#0F5B01',
-    '#2ecc71',
-    'rgb(240,147,145)',
-    '#f39c12'
+    labels: labels,
+    datasets: [
+        {
+            backgroundColor: [
+                "#0F5B01",
+                "#2ecc71",
+                "rgb(240,147,145)",
+                "#f39c12",
+            ],
+            borderColor: "#f1c40f",
+            data: [
+                resultbyCategory[0].value,
+                resultbyCategory[1].value,
+                resultbyCategory[2].value,
+                resultbyCategory[3].value,
+            ],
+        },
     ],
-    borderColor: '#f1c40f',
-    data: [resultbyCategory[0].value, resultbyCategory[1].value, resultbyCategory[2].value, resultbyCategory[3].value]
-}]
 };
 
 const config = {
-type: 'doughnut',
-data: data,
-options: {}
+    type: "doughnut",
+    data: data,
+    options: {},
 };
 
-const myChart = new Chart(
-document.getElementById('myChart'),
-config
-);
+const myChart = new Chart(document.getElementById("myChart"), config);
